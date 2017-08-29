b3c0d79093547c9c4de7dcb864867bb764651f8c
khalidrs1903 <31385089+khalidrs1903@users.noreply.github.com>
2017-08-29 14:17:10 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function creatTemplate (data) {
 var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
-    res.send(counter.tostring());
+    res.send(counter.toString());
 });
 
 app.get('/', function (req, res) {
